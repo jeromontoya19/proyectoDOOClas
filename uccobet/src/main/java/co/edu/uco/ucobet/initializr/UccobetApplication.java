c551692d74673108e867ff910f74a118782e342b
jeromontoya19 <jeronimo.montoya6916@uco.net.co>
2024-10-09 16:13:11 -0500

    add ucobet whit DAO, implement spring web and sql

---
+++ uccobet/src/main/java/co/edu/uco/ucobet/initializr/UccobetApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.ucobet.initializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UccobetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UccobetApplication.class, args);
+	}
+
+}
---
+++ uccobet/src/test/java/co/edu/uco/ucobet/initializr/UccobetApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.ucobet.initializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UccobetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
